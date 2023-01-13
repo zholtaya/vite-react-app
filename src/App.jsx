1de162a622d4e5dc58924a6e9ee784e545fe16b3
leysan <zholtayachashka@gmail.com>
2023-01-13 14:30:07 +0300

    Init react app

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Header from './Header'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <Header/>
+      <Header/>
+      <Header/>
+      <Header/>
+      <Header/>
+      <Header/>
+      <p>rrrr</p>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,28 @@
+// function Header(){
+//     return (
+//       <header>
+//         <h1>header</h1>
+//       <p>meowmeow</p>
+
+//       </header>  
+//     );
+// }
+// export default Header;
+// export function Header() {
+//     return (
+//         <header>
+//             <h1>header</h1>
+//             <p>meowmeow</p>
+
+//         </header>
+//     );
+// }
+const Header =()=>{
+    return (
+        <header>
+            <h1>header</h1>
+            <p>meowmeow</p>
+
+        </header>
+    );
+}
